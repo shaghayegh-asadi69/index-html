e792990fa4867be9d61b6e0f285a7f23bcf2959a
Shaghayegh Asadi <shaghayegh.asadi69@yahoo.com>
2023-06-08 16:26:02 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//let userColor = prompt("Choose a color!")
+
+//document.body.style.backgroundColor = userColor;
+//console.log(userColor);
+
+//if (userColor === "pink") {document.write("same here")};
+
+const welcomingMsg = alert("Welcome to my page ");
+console.log(welcomingMsg);
+
+//document.write("Hello " + userName + " thank you for trying to save our World");
+
+let text;
+if (confirm ("Are you Sure You Want To Continue?!") == true) {
+ text = "You pressed OK!";
+} else {
+  text = "You canceled!";}
